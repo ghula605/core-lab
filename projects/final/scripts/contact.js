beaec6d2ba841d96d3eae76ad3e60b5c49b4ed7d
ghula605 <60526181+ghula605@users.noreply.github.com>
2020-05-10 20:47:22 +0400

    final update

---
+++ projects/final/scripts/contact.js
@@ -0,0 +1,5 @@
+// This is where the javascript will live.
+// Using Chrome developer tools, if you look at the console, you can see the message appearing on the console.
+
+console.log('Is the scripts file working?');
+
