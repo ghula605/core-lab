d28f7104910ad056ba32b7348cf13551d1a15d0c
ghula605 <60526181+ghula605@users.noreply.github.com>
2020-05-12 18:38:13 +0400

    update

--- projects/final/scripts/grant.js
+++ projects/final/scripts/grant.js
@@ -1,8 +1,6 @@
 // This is where the javascript will live.
 // Using Chrome developer tools, if you look at the console, you can see the message appearing on the console.
 
-console.log('Is the scripts file working?');
-
 $( function() {
 
     // code will go inside here 
