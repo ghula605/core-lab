3d4ce1b5e25dc8a376641fbf44dd4486e49ad40b
ghula605 <60526181+ghula605@users.noreply.github.com>
2020-05-12 06:41:04 +0400

    update

--- projects/final/scripts/final.js
+++ projects/final/scripts/final.js
@@ -1,5 +1,22 @@
 // This is where the javascript will live.
 // Using Chrome developer tools, if you look at the console, you can see the message appearing on the console.
 
-console.log('Is the scripts file working?');
+$( function() {
 
+    // code will go inside here 
+
+    //toggle info
+    $( ".corona-update" ).click( function() {
+
+        $(".container").toggleClass("show");
+    
+    })
+
+        //toggle info
+    $( ".corona-update" ).click( function() {
+
+        $(".container-mask").toggleClass("show2");
+    
+    })
+
+})
