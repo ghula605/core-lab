cffd7d74ebecb78057d7ffb780db789e774040b0
ghula605 <60526181+ghula605@users.noreply.github.com>
2020-05-12 18:55:54 +0400

    update

--- projects/final/scripts/gallery.js
+++ projects/final/scripts/gallery.js
@@ -1,5 +1,15 @@
 // This is where the javascript will live.
 // Using Chrome developer tools, if you look at the console, you can see the message appearing on the console.
 
-console.log('Is the scripts file working?');
+$( function() {
 
+    // code will go inside here 
+
+    //toggle info
+    $( ".burger" ).click( function() {
+
+        $(".menu").toggleClass("show");
+    
+    })
+
+})
